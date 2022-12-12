5cbf893769837b09dab60c89aa03c8112e1a4004
Petro Telishevskyi <ptelishewsky@gmail.com>
2022-12-12 23:28:19 +0200

    Change app.py

--- app.py
+++ app.py
@@ -3,8 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello world, from Flask"
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+def index():
+    return "Hello this is the new version!"
