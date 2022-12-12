b98755899fde4149db974586f292995702948eca
Petro Telishevskyi <ptelishewsky@gmail.com>
2022-12-12 23:57:08 +0200

    Change app.py, add library requests

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,5 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def getInfo():
     data = request.get_data()
+
+
 if __name__ == '__main__':
     app.run(debug=True)
