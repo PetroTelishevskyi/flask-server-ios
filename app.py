68bd37053f290991ad2ab5db768863a04af62b4b
Petro Telishevskyi <ptelishewsky@gmail.com>
2022-12-13 21:28:53 +0200

    corret 404

--- app.py
+++ app.py
@@ -7,27 +7,27 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     
-    if request.method == 'POST':
-        print('post request received')
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            print('file name is valid and saving')
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return jsonify({'success': 1})
-        else:
-            print('file failed to save')
-            return jsonify({'success': 2})
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+    
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+        
+        if file:
+            song = AudioSegment.from_file(file, format="m4a")
+            song.export("filename.mp3", format="mp3")
+          
+            return make_response(
+                    jsonify(
+                        {"ruword": 'прєвєт', "uaword": 'привіт'}
+                    ), 200
+            )
         
 
-def allowed_file(filename):
-    print('in allowed_file')
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1] in app.config['AllOWED_EXTENSIONS']
-
-#Config settings
-UPLOAD_FOLDER = './Users/User/Desktop/flask server'
-ALLOWED_EXTENSIONS = set(['m4a'])       
 
 if __name__ == '__main__':
     app.run(debug=True)
