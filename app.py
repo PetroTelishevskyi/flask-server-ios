694e0dd7f9c6b4aabee1885e031c8f4c14d84231
Petro Telishevskyi <ptelishewsky@gmail.com>
2022-12-13 20:08:40 +0200

    test response json

--- app.py
+++ app.py
@@ -6,16 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    FRAME_RATE = 16000
-    CHANNELS=1
-
-    modelUa = Model(model_name="vosk-model-uk-v3")
-    modelRu = Model(model_name="vosk-model-small-ru-0.22")
-            # For a smaller download size, use model = Model(model_name="vosk-model-small-en-us-0.15")
-    recUa = KaldiRecognizer(modelUa, FRAME_RATE)
-    recRu = KaldiRecognizer(modelRu, FRAME_RATE)
-    recUa.SetWords(True)
-    recRu.SetWords(True)
+    
     if request.method == "POST":
         print("FORM DATA RECEIVED")
     
@@ -29,29 +20,7 @@ def index():
         if file:
             song = AudioSegment.from_file(file, format="mp4")
             song.export("filename.mp3", format="mp3")
-            song = song.set_channels(CHANNELS)
-            song = song.set_frame_rate(FRAME_RATE)
-
-            recUa.AcceptWaveform(song.raw_data)
-            resultUa = recUa.Result()
-            recRu.AcceptWaveform(song.raw_data)
-            resultRu = recRu.Result()
-            textUa = json.loads(resultUa)
-            textRu = json.loads(resultRu)
-
-            tUA = textUa['result']
-            tRU = textRu['result']
-
-            arrRU = []
-            translation = []
-            translator= Translator(from_lang="ru",to_lang="uk")
-            for i in range(len(text1['result'])):
-                ttUa = tUA[i]
-                ttRu = tRU[i]
-                if ttUA['conf'] < ttRu['conf']:
-                    arrRU.append(ttRu['word'])
-                    translation.append(translator.translate(ttRu['word']))
-    
+          
     return make_response(
         jsonify(
             {"ruword": 'прєвєт', "uaword": 'привіт' }
