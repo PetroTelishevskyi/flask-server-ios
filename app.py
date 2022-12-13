6d30130a56b10c4b795486e8ee382a018b8b535e
Petro Telishevskyi <ptelishewsky@gmail.com>
2022-12-13 18:55:09 +0200

    change app.py, where add request with audio

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def index():
-    data = request.get_data()
-    print(data)
-    return data
-    
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
 
 if __name__ == '__main__':
     app.run(debug=True)
