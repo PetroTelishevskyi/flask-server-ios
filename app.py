736bf800f6b1a9f4f46c3e8a36f9efc752f6ca26
Petro Telishevskyi <ptelishewsky@gmail.com>
2022-12-13 21:59:26 +0200

    correct error status 500 version4

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def upload_file():
         
     return make_response(
             jsonify(
-                        {"ruword": 'прєвєт', "uaword": 'привіт', 'song': file}
+                        {"ruword": 'прєвєт', "uaword": 'привіт', 'song': filename}
                     ), 200
             )
         
