c6e1ce64072b75321cc8217e960db435418cc414
Petro Telishevskyi <ptelishewsky@gmail.com>
2022-12-13 21:50:00 +0200

    correct error status 500 version2

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            print('No file part')
             return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
+            print('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
