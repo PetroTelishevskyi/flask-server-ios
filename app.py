7a2642252701167e5777009c955bfe145b41b814
Petro Telishevskyi <ptelishewsky@gmail.com>
2022-12-13 22:54:04 +0200

    work version3

--- app.py
+++ app.py
@@ -2,28 +2,23 @@ from flask import Flask, request, redirect, jsonify, make_response
 from vosk import Model, KaldiRecognizer
 from pydub import AudioSegment
 from translate import Translator
+
 app = Flask(__name__)
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    
     if request.method == "POST":
-        print("FORM DATA RECEIVED")
-    
         if "file" not in request.files:
-            return redirect(request.url)
+            return {'success': False, 'reason': 'No file'}
 
         file = request.files["file"]
         if file.filename == "":
-            return redirect(request.url)
-        
-          
-    return make_response(
-        jsonify(
-            {"ruword": 'прєвєт', "uaword": 'привіт' }
-        ), 200
-    )
-        
+            return {'success': False, 'reason': 'Empty filename'}
+
+        return {'success': True, 'file': file.filename}
+
+    return 'OK'
 
 
 if __name__ == '__main__':
